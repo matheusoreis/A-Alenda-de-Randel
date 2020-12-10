df03673606679f793328555f66f044b91a77ddb0
kies011 <47448206+kies011@users.noreply.github.com>
2020-12-09 22:04:12 -0300

    Configurações dos prefabs #1

---
+++ Assets/Scripts/FadeBack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeBack : MonoBehaviour {
+
+    SpriteRenderer spriteRenderer;
+
+    public float transparent = 0.6f;
+
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+
+        if (collision.gameObject.CompareTag("Player")){
+            spriteRenderer.color = new Color(1f, 1f, 1f, transparent);
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D collision) {
+        spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+    }
+
+}
