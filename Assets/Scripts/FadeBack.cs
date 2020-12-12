c7626bc10416371f4d1b5df2b571d3b60497f83a
kies011 <47448206+kies011@users.noreply.github.com>
2020-12-12 14:18:10 -0300

    Configuração do mapa 01
    Colider adicionado

--- Assets/Scripts/FadeBack.cs
+++ Assets/Scripts/FadeBack.cs
@@ -1,27 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class FadeBack : MonoBehaviour {
 
     SpriteRenderer spriteRenderer;
+    Tilemap tileRenderer;
 
-    public float transparent = 0.6f;
+    public bool IsTileChoise;
+
+    public float Transparent = 0.6f;
 
     void Start() {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        tileRenderer = GetComponent<Tilemap>();
     }
 
     void OnTriggerEnter2D(Collider2D collision) {
 
-        if (collision.gameObject.CompareTag("Player")){
-            spriteRenderer.color = new Color(1f, 1f, 1f, transparent);
+        // Condição para verificar se é um Sprite ou Tilemap
+        if (IsTileChoise == true) {
+            if (collision.gameObject.CompareTag("Player")) {
+                tileRenderer.color = new Color(1f, 1f, 1f, Transparent);
+            }
+        }
+        else {
+            if (collision.gameObject.CompareTag("Player")) {
+                spriteRenderer.color = new Color(1f, 1f, 1f, Transparent);
+            }
         }
 
     }
 
     void OnTriggerExit2D(Collider2D collision) {
-        spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+
+        // Condição para verificar se é um Sprite ou Tilemap
+        if (IsTileChoise == true) {
+            tileRenderer.color = new Color(1f, 1f, 1f, 1f);
+        }
+        else {
+            spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+        }
+        
     }
 
 }
